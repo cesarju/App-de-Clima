a33120606866b4a7e27aa52fefa223a5ccf5cd2a
Cesar <cesarju.cc@gmail.com>
2020-05-03 21:12:37 -0400

    clase cuidad

--- app/src/main/java/com/example/myapplication/Ciudades.kt
+++ app/src/main/java/com/example/myapplication/Ciudades.kt
@@ -1,12 +1,33 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class Ciudades : AppCompatActivity() {
+
+    val TAG = "com.example.myapplication.ciudades.CIUDAD"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
+
+        val btLaPaz = findViewById<Button>(R.id.btLaPaz)
+        val btCbba = findViewById<Button>(R.id.btCbb)
+
+        btLaPaz.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG, "Ciudad de La Paz")
+            startActivity(intent)
+        })
+        btCbba.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG,"Ciudad de Cochabamba")
+            startActivity(intent)
+        })
+
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var tvCiudad:TextView? = null
+    var tvGrados:TextView? = null
+    var tvEstatus:TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvCiudad = findViewById(R.id.tCiudad)
+        tvGrados = findViewById(R.id.tGrados)
+        tvEstatus = findViewById(R.id.tEstatus)
+
+        val ciudad = intent.getStringExtra("com.example.myapplication.ciudades.CIUDAD")
+        Toast.makeText(this, ciudad, Toast.LENGTH_SHORT).show()
+
+        //Objetos de la clase Ciudad.kt
+        val ciudadLP = Ciudad("Ciudad de La Paz", 15,"Soleado")
+        val ciudadCbb = Ciudad("Ciudad de Cochabamba",10, "Lluvioso")
+
+        if(ciudad == "Ciudad de La Paz"){
+            //Mostrar informacion de la ciudad de La Paz
+            tvCiudad?.text = ciudadLP.nombre
+            tvGrados?.text = ciudadLP.grados.toString() + "°"
+            tvEstatus?.text = ciudadLP.estatus
+
+        }else if (ciudad == "Ciudad de Cochabamba"){
+            // Mostrar informacion de la ciudad de Cocahbamba
+
+            tvCiudad?.text = ciudadCbb.nombre
+            tvGrados?.text = ciudadCbb.grados.toString() + "°"
+            tvEstatus?.text = ciudadCbb.estatus
+        }else{
+            Toast.makeText(this, "No se encontro la informacion", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
